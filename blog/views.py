6896d9ddfcd4cd9260a62900e263558b69780cab
Roman Mykhailyshyn <romas.m@mail.ru>
2019-12-25 17:55:07 +0200

    added post publish method

--- blog/views.py
+++ blog/views.py
@@ -57,6 +57,15 @@ class DraftListView(LoginRequiredMixin, ListView):
 ###############################################################################
 ###############################################################################
 @login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+
+
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -84,4 +93,3 @@ def comment_remove(request, pk):
     post_pk = comment.post.pk
     comment.delete()
     return redirect('post_detail', pk=post_pk)
-
